14f1ae6b92d5177c14149f5b14b63115dfe42145
Ciprian Lazar <ciprian.lazar@ivfuture.uk>
2023-05-15 23:14:37 +0300

    Add maps

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://28ea-5-2-209-123.ngrok-free.app'
+    baseURL: 'http://aaa3-5-13-130-200.ngrok-free.app'
 });
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,8 +1,43 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import '../_mockLocation';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet} from 'react-native';
+import {Text} from 'react-native-elements';
+import {SafeAreaView} from "react-native-safe-area-context";
+import {Accuracy, requestForegroundPermissionsAsync, watchPositionAsync} from "expo-location";
+import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
-  return <Text style={{ fontSize: 48 }}>TrackCreateScreen</Text>;
+    const [err, setErr] = useState(null);
+
+    const startWatching = async () => {
+        try {
+            await requestForegroundPermissionsAsync();
+            await watchPositionAsync({
+                    accuracy: Accuracy.BestForNavigation,
+                    timeInterval: 1000,
+                    distanceInterval: 10
+                },
+                (location) => {
+                    console.log(location);
+                }
+            )
+        } catch (e) {
+            setErr(e);
+        }
+    };
+
+    useEffect(() => {
+        startWatching();
+    }, []);
+
+    return (
+        <SafeAreaView forceInset={{top: 'always'}}>
+            <Text h3>TrackCreateScreen</Text>
+            <Map/>
+            {err ? <Text>Please enable location services</Text> : null}
+        </SafeAreaView>
+    );
+
 };
 
 const styles = StyleSheet.create({});
