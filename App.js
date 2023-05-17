a9ccb9017df52a2e38b7f62bf6d4679e76b57a15
Ciprian Lazar <ciprian.lazar@ivfuture.uk>
2023-05-17 23:25:01 +0300

    Draw current location as a circle

--- App.js
+++ App.js
@@ -1,43 +1,46 @@
 import React from "react";
-import { createAppContainer, createSwitchNavigator } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
+import {createAppContainer, createSwitchNavigator} from "react-navigation";
+import {createStackNavigator} from "react-navigation-stack";
+import {createMaterialBottomTabNavigator} from "react-navigation-material-bottom-tabs";
 import AccountScreen from "./src/screens/AccountScreen";
 import SigninScreen from "./src/screens/SigninScreen";
 import SignupScreen from "./src/screens/SignupScreen";
 import TrackCreateScreen from "./src/screens/TrackCreateScreen";
 import TrackDetailScreen from "./src/screens/TrackDetailScreen";
 import TrackListScreen from "./src/screens/TrackListScreen";
-import { Provider as AuthProvider } from "./src/context/AuthContext";
-import { setNavigator } from "./src/navigationRef";
+import {Provider as AuthProvider} from "./src/context/AuthContext";
+import {Provider as LocationProvider} from "./src/context/locationContext";
+import {setNavigator} from "./src/navigationRef";
 import ResolveAuthScreen from "./src/screens/ResolveAuthScreen";
 
 const switchNavigator = createSwitchNavigator({
-  ResolveAuth: ResolveAuthScreen,
-  loginFlow: createStackNavigator({
-    Signup: SignupScreen,
-    Signin: SigninScreen,
-  }),
-  mainFlow: createMaterialBottomTabNavigator({
-    trackListFlow: createStackNavigator({
-      TrackList: TrackListScreen,
-      TrackDetail: TrackDetailScreen,
+    ResolveAuth: ResolveAuthScreen,
+    loginFlow: createStackNavigator({
+        Signup: SignupScreen,
+        Signin: SigninScreen,
     }),
-    TrackCreate: TrackCreateScreen,
-    Account: AccountScreen,
-  }),
+    mainFlow: createMaterialBottomTabNavigator({
+        trackListFlow: createStackNavigator({
+            TrackList: TrackListScreen,
+            TrackDetail: TrackDetailScreen,
+        }),
+        TrackCreate: TrackCreateScreen,
+        Account: AccountScreen,
+    }),
 });
 
 const App = createAppContainer(switchNavigator);
 
 export default () => {
-  return (
-    <AuthProvider>
-      <App
-        ref={(navigator) => {
-          setNavigator(navigator);
-        }}
-      />
-    </AuthProvider>
-  );
+    return (
+        <LocationProvider>
+            <AuthProvider>
+                <App
+                    ref={(navigator) => {
+                        setNavigator(navigator);
+                    }}
+                />
+            </AuthProvider>
+        </LocationProvider>
+    );
 };
--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://aaa3-5-13-130-200.ngrok-free.app'
+    baseURL: 'http://cf54-5-13-130-200.ngrok-free.app'
 });
