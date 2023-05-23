a4a66645957e39e2e973464dac4698c07ce5ad95
Ciprian Lazar <ciprian.lazar@ivfuture.uk>
2023-05-23 22:24:06 +0300

    Implement use location hook

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://cf54-5-13-130-200.ngrok-free.app'
+    baseURL: 'http://aba3-5-13-130-200.ngrok-free.app'
 });
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,36 +1,16 @@
 import '../_mockLocation';
-import React, {useEffect, useState, useContext} from 'react';
+import React, {useContext} from 'react';
 import {StyleSheet} from 'react-native';
 import {Text} from 'react-native-elements';
 import {SafeAreaView} from "react-native-safe-area-context";
-import {Accuracy, requestForegroundPermissionsAsync, watchPositionAsync} from "expo-location";
+import {withNavigationFocus} from "react-navigation";
 import Map from '../components/Map';
 import {Context as LocationContext} from "../context/locationContext";
+import useLocation from "../hooks/useLocation";
 
-const TrackCreateScreen = () => {
+const TrackCreateScreen = ({isFocused}) => {
     const { addLocation } = useContext(LocationContext);
-    const [err, setErr] = useState(null);
-
-    const startWatching = async () => {
-        try {
-            await requestForegroundPermissionsAsync();
-            await watchPositionAsync({
-                    accuracy: Accuracy.BestForNavigation,
-                    timeInterval: 1000,
-                    distanceInterval: 10
-                },
-                (location) => {
-                    addLocation(location);
-                }
-            )
-        } catch (e) {
-            setErr(e);
-        }
-    };
-
-    useEffect(() => {
-        startWatching();
-    }, []);
+    const [err] = useLocation(addLocation);
 
     return (
         <SafeAreaView forceInset={{top: 'always'}}>
@@ -39,9 +19,8 @@ const TrackCreateScreen = () => {
             {err ? <Text>Please enable location services</Text> : null}
         </SafeAreaView>
     );
-
 };
 
 const styles = StyleSheet.create({});
 
-export default TrackCreateScreen;
+export default withNavigationFocus(TrackCreateScreen);
