a6acfced8dfd4e169659b91cf3e399d77164ef29
Ciprian Lazar <ciprian.lazar@ivfuture.uk>
2023-05-30 22:33:32 +0300

    Implement use location hook rev1.5

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://aba3-5-13-130-200.ngrok-free.app'
+    baseURL: 'http://f9b2-5-13-130-200.ngrok-free.app'
 });
--- src/context/TrackContext.js
+++ src/context/TrackContext.js
@@ -10,7 +10,8 @@ const trackReducer = (state, action) => {
 const fetchTracks = dispatch => () => {
 };
 
-const createTrack = dispatch => () => {
+const createTrack = dispatch => (name, locations) => {
+    console.log(name, locations.length);
 };
 
 export const {Provider, Context} = createDataContext(
